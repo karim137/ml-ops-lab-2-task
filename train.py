67ab45497cedea5e21a461bd6b1470b9712ca818
karim 137 <k.mohamed2247@nu.edu.eg>
2025-10-15 23:45:43 +0300

    svm code

--- train.py
+++ train.py
@@ -1,24 +1,29 @@
-
-
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 import joblib
 import os
 
+# -----------------------------
+# File paths
+# -----------------------------
 TRAIN_PATH = r"C:\Users\hankerz\OneDrive\ml-ops-lab-2-task\train.csv"
 MODEL_PATH = r"C:\Users\hankerz\OneDrive\ml-ops-lab-2-task\model.pkl"
 
+# -----------------------------
+# Load training data
+# -----------------------------
 if not os.path.exists(TRAIN_PATH):
     raise FileNotFoundError(f"Train file not found: {TRAIN_PATH}")
 
 df = pd.read_csv(TRAIN_PATH)
 
-X = df.drop("Outcome", axis=1)  
-y = df["Outcome"]              
+X = df.drop("Outcome", axis=1)  # Features
+y = df["Outcome"]               # Target
 
-
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+# -----------------------------
+# Train SVM model
+# -----------------------------
+model = SVC(kernel='rbf', probability=True, random_state=42)
 model.fit(X, y)
 
 # -----------------------------
